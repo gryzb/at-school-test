8ca8e2800a8fba106ef8fb467a05fc5dc2ac2465
dekin1337 <dekin1337@gmail.com>
2020-09-16 15:33:34 +0300

    asddsa

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+import somepackage.Other;
+import somepackage.Some;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Other.saySomthing();
+        Some some = new Some("String for constructor");
+        some.setNoMoreSecrets("Hello");
+        System.out.println(some.getNoMoreSecrets());
+        some.setNoMoreSecrets(123);
+        System.out.println("I'm already string: " + some.getNoMoreSecrets());
+        System.out.println(some.getSecret());
+        some.getSecret(Boolean.TRUE);
+        System.out.println(some.getSecret());
+        System.out.println(some.toString());
+        System.out.println(some.count());
+        System.out.println(some.getДаТакТожеМожно());
+        some.soTellMeAboutInterfaces();
+    }
+}
